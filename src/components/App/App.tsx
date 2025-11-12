37795f8b895388c6376fb5db57ce08bbaca0e87f
bndnt <67003594+bndnt@users.noreply.github.com>
2025-11-11 23:13:12 -0500

    stale time for caches

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPerson } from "../../services/swapiService";
+import { useState } from "react";
 function App() {
+  const [counter, setCounter] = useState<number>(1);
   const { data, isError, isLoading } = useQuery({
-    queryKey: ["cherecter"],
-    queryFn: fetchPerson,
+    queryKey: ["cherecter", counter],
+    queryFn: () => fetchPerson(counter),
+    enabled: counter > 0,
+    staleTime: 60 * 1000,
+    // 1 minute
   });
 
   console.log({ data, isLoading, isError });
 
   return (
     <>
+      <button type="button" onClick={() => setCounter((prev) => prev - 1)}>
+        -
+      </button>
+      <p>counter:{counter}</p>
+      <button type="button" onClick={() => setCounter((prev) => prev + 1)}>
+        +
+      </button>
       <h1>hello </h1>
       {isLoading && <p>Loading....</p>}
       {isError && <p>Oops.. try again later</p>}
--- src/services/swapiService.ts
+++ src/services/swapiService.ts
@@ -4,8 +4,8 @@ import axios from "axios";
 //   const response = await axios.get(`https://swapi.info/api/people/1`);
 //   return response.data;
 // };
-export const fetchPerson = async () => {
-  const { data } = await axios.get(`https://swapi.info/api/people/1`);
+export const fetchPerson = async (id: number) => {
+  const { data } = await axios.get(`https://swapi.info/api/people/${id}`);
   //   console.log(data.name);
 
   return data;
